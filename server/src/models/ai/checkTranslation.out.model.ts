5518df6edd6f92bf018fd87db41706868a86120e
Andrew Kozinsky <andkozinskiy@yandex.ru>
2025-04-24 09:55:33 +0500

    Refactor CheckTranslationOutModel to use GraphQL union type for handling success and error responses

--- server/src/models/ai/checkTranslation.out.model.ts
+++ server/src/models/ai/checkTranslation.out.model.ts
@@ -1,10 +1,27 @@
-import { Field, ObjectType } from '@nestjs/graphql'
+import { Field, ObjectType, createUnionType } from '@nestjs/graphql'
 
 @ObjectType()
-export class CheckTranslationOutModel {
+export class ErrorResult {
+	@Field(() => String)
+	error: string
+}
+
+@ObjectType()
+export class SuccessResult {
 	@Field(() => Boolean)
 	correct: boolean
 
 	@Field(() => String)
 	analysis: string
 }
+
+export const CheckTranslationOutModel = createUnionType({
+	name: 'CheckTranslationOutModel',
+	types: () => [SuccessResult, ErrorResult],
+	resolveType(value) {
+		if ('error' in value) {
+			return ErrorResult
+		}
+		return SuccessResult
+	},
+})
