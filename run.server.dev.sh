d500fdfb687e928dece240347a83742f6db6f778
Andrew Kozinsky <andkozinskiy@yandex.ru>
2025-10-19 20:11:48 +0500

    v1 a try to run on the server 6

--- run.server.dev.sh
+++ run.server.dev.sh
@@ -1,5 +1,9 @@
 git pull
 
+# Останавливаем и удаляем существующие контейнеры
+docker compose -f docker-compose.server.develop.yml --env-file .env.serverdevelop down 2>/dev/null || true
+
+# Запускаем контейнеры
 (docker compose -f docker-compose.server.develop.yml --env-file .env.serverdevelop up --build) &
 sleep 10
 cd server || exit
