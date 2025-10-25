95f4a02308ce9efd7856d1925b46885612cfc065
Andrew Kozinsky <andkozinskiy@yandex.ru>
2025-10-25 16:50:39 +0500

    v1 update workflow.yml

--- run.server.dev.sh
+++ run.server.dev.sh
@@ -4,7 +4,4 @@ git pull
 docker compose -f docker-compose.server.develop.yml --env-file .env.serverdevelop down 2>/dev/null || true
 
 # Запускаем контейнеры
-(docker compose -f docker-compose.server.develop.yml --env-file .env.serverdevelop up --build) &
-sleep 10
-cd server || exit
-npm run migrate:dev
+docker compose -f docker-compose.server.develop.yml --env-file .env.serverdevelop up --build
