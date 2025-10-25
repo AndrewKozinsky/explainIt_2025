b5b0231c2c14d806cf8c53629974efcd04eb22ec
Andrew Kozinsky <andkozinskiy@yandex.ru>
2025-10-25 20:55:58 +0500

    v1 a try to set up cookie 2

--- face/graphql/apollo.ts
+++ face/graphql/apollo.ts
@@ -1,6 +1,6 @@
 import { InMemoryCache, ApolloClient, HttpLink, from } from '@apollo/client'
 import { onError } from '@apollo/client/link/error'
-import { isServerComponent } from '../utils/utils'
+import { isServerComponent } from 'utils/utils'
 
 // Error handling link
 const errorLink = onError(({ graphQLErrors, networkError }) => {
@@ -14,27 +14,6 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
 		console.error(`[Network error]: ${networkError}`)
 	}
 })
-
-// Create a function to get the appropriate host based on environment
-const getHost = () => {
-	// When running on the server, use the Docker service name
-	if (isServerComponent()) {
-		// return 'explain-server:3001'
-		return 'localhost'
-	}
-	// When running in the browser, use Nginx as a proxy
-	return 'localhost'
-}
-
-// Create a function to get the appropriate URI based on environment
-const getUri = () => {
-	// When running on the server, connect directly to the server container
-	if (isServerComponent()) {
-		return `http://${getHost()}/graphql`
-	}
-	// When running in the browser, use Nginx as a proxy
-	return '/graphql'
-}
 
 // HTTP link
 const httpLink = new HttpLink({
@@ -59,3 +38,34 @@ export const apolloClient = new ApolloClient({
 })
 
 export default apolloClient
+
+// Create a function to get the appropriate URI based on environment
+/*function getUri() {
+	// When running on the server, connect directly to the server container
+	if (isServerComponent()) {
+		return `http://${getHost()}/graphql`
+	}
+	// When running in the browser, use Nginx as a proxy
+	return '/graphql'
+}*/
+function getUri() {
+	// When running on the server, connect directly to the server container
+	if (isServerComponent()) {
+		return `https://${getHost()}/graphql`
+	}
+	// When running in the browser, use Nginx as a proxy
+	return '/graphql'
+}
+
+// Create a function to get the appropriate host based on environment
+/*function getHost() {
+	// When running on the server, use the Docker service name
+	if (isServerComponent()) {
+		return 'localhost'
+	}
+	// When running in the browser, use Nginx as a proxy
+	return 'localhost'
+}*/
+function getHost() {
+	return 'dev.explainit.ru'
+}
