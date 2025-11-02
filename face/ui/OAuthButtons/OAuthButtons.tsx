e62c4ce778173044dd449c3832d766f9fa7e0014
Andrew Kozinsky <andkozinskiy@yandex.ru>
2025-11-02 19:06:16 +0500

    Little text improvements

--- face/ui/OAuthButtons/OAuthButtons.tsx
+++ face/ui/OAuthButtons/OAuthButtons.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Paragraph from 'ui/Paragraph/Paragraph'
 import Button from '../formRelated/buttons/Button/Button'
 import { useGetOnButtonClick } from './fn/onButtonClick'
 import './OAuthButtons.scss'
@@ -10,9 +11,13 @@ function OAuthButtons() {
 
 	return (
 		<div className='oauth'>
-			{/*<Button onClick={onGitHubClick}>GitHub</Button>*/}
-			{/*<Button onClick={onGoogleClick}>Google</Button>*/}
-			<Button onClick={onYandexClick}>Yandex</Button>
+			<p>Быстрый вход через социальную сеть:</p>
+			<div className='oauth__buttons'>
+				{/*<Button onClick={onGitHubClick}>GitHub</Button>*/}
+				{/*<Button onClick={onGoogleClick}>Google</Button>*/}
+				<Button onClick={onYandexClick}>Yandex</Button>
+			</div>
+			<Paragraph fontSize={14}>При регистрации получаете приветственный бонус на баланс</Paragraph>
 		</div>
 	)
 }
