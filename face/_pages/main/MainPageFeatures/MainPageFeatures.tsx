e82c7210a6c90159338b1434f3c2f96914114cfb
Andrew Kozinsky <andkozinskiy@yandex.ru>
2026-05-10 20:01:35 +0500

    Add scroll buttons in MainPageFeatures

--- face/_pages/main/MainPageFeatures/MainPageFeatures.tsx
+++ face/_pages/main/MainPageFeatures/MainPageFeatures.tsx
@@ -1,47 +1,65 @@
-import React from 'react'
+'use client'
+
+import React, { useRef } from 'react'
 import { filesUrls } from 'utils/filesUrls'
 import './MainPageFeatures.scss'
 
 function MainPageFeatures() {
+	const scrollContainerRef = useRef<HTMLDivElement>(null)
+
+	const scrollBy = (amount: number) => {
+		scrollContainerRef.current?.scrollBy({ left: amount, behavior: 'smooth' })
+	}
+
 	return (
-		<div className='main-page-features'>
-			<div className='main-page-feature'>
-				<img
-					src={filesUrls.mainPage.features.phone['1x']}
-					srcSet={`${filesUrls.mainPage.features.phone['2x']} 2x`}
-					className='main-page-feature__image'
-					alt='phone'
-				/>
-				<p className='main-page-feature__text'>
-					<span className='main-page-feature__text--bold'>Получайте перевод</span> предложения, фразы и
-					примеры использования нажимая на слово прямо в тексте.
-				</p>
+		<div className='main-page-features-wrapper'>
+			<div className='main-page-features' ref={scrollContainerRef}>
+				<div className='main-page-feature'>
+					<img
+						src={filesUrls.mainPage.features.phone['1x']}
+						srcSet={`${filesUrls.mainPage.features.phone['2x']} 2x`}
+						className='main-page-feature__image'
+						alt='phone'
+					/>
+					<p className='main-page-feature__text'>
+						<span className='main-page-feature__text--bold'>Получайте перевод</span> предложения, фразы и
+						примеры использования нажимая на слово прямо в тексте.
+					</p>
+				</div>
+				<div className='main-page-feature__sign'>✦</div>
+				<div className='main-page-feature'>
+					<img
+						src={filesUrls.mainPage.features.dictionary['1x']}
+						srcSet={`${filesUrls.mainPage.features.dictionary['2x']} 2x`}
+						className='main-page-feature__image'
+						alt='phone'
+					/>
+					<p className='main-page-feature__text'>
+						<span className='main-page-feature__text--bold'>Работайте с новыми фразами:</span> предложения,
+						читайте транскрипцию, слушайте произношение и сохраняйте в словарь.
+					</p>
+				</div>
+				<div className='main-page-feature__sign'>✦</div>
+				<div className='main-page-feature'>
+					<img
+						src={filesUrls.mainPage.features.assistant['1x']}
+						srcSet={`${filesUrls.mainPage.features.assistant['2x']} 2x`}
+						className='main-page-feature__image'
+						alt='phone'
+					/>
+					<p className='main-page-feature__text'>
+						<span className='main-page-feature__text--bold'>Спрашивайте ассистента</span> про грамматику,
+						нюансы употребления и смысл в контексте.
+					</p>
+				</div>
 			</div>
-			<div className='main-page-feature__sign'>✦</div>
-			<div className='main-page-feature'>
-				<img
-					src={filesUrls.mainPage.features.dictionary['1x']}
-					srcSet={`${filesUrls.mainPage.features.dictionary['2x']} 2x`}
-					className='main-page-feature__image'
-					alt='phone'
-				/>
-				<p className='main-page-feature__text'>
-					<span className='main-page-feature__text--bold'>Работайте с новыми фразами:</span> предложения,
-					читайте транскрипцию, слушайте произношение и сохраняйте в словарь.
-				</p>
-			</div>
-			<div className='main-page-feature__sign'>✦</div>
-			<div className='main-page-feature'>
-				<img
-					src={filesUrls.mainPage.features.assistant['1x']}
-					srcSet={`${filesUrls.mainPage.features.assistant['2x']} 2x`}
-					className='main-page-feature__image'
-					alt='phone'
-				/>
-				<p className='main-page-feature__text'>
-					<span className='main-page-feature__text--bold'>Спрашивайте ассистента</span> про грамматику, нюансы
-					употребления и смысл в контексте.
-				</p>
+			<div className='main-page-features__controls'>
+				<button className='scroll-button' onClick={() => scrollBy(-336)}>
+					←
+				</button>
+				<button className='scroll-button' onClick={() => scrollBy(336)}>
+					→
+				</button>
 			</div>
 		</div>
 	)
