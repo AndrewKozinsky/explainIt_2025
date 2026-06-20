0b122590028f76cd24910df54e8806d05349d78b
Andrew Kozinsky <andkozinskiy@yandex.ru>
2026-06-20 15:39:31 +0500

    10

--- face/_pages/media/video/editPrivateVideo/GenerateSubtitlesBlock/GenerateSubtitlesBlock.tsx
+++ face/_pages/media/video/editPrivateVideo/GenerateSubtitlesBlock/GenerateSubtitlesBlock.tsx
@@ -1,4 +1,6 @@
 import GenerateSubtitlesButton from '../GenerateSubtitlesButton/GenerateSubtitlesButton'
+import mainConfig from 'сonsts/mainConfig'
+import './GenerateSubtitlesBlock.scss'
 
 type GenerateSubtitlesBlockProps = {
 	isFormDisabled: boolean
@@ -8,9 +10,21 @@ function GenerateSubtitlesBlock(props: GenerateSubtitlesBlockProps) {
 	const { isFormDisabled } = props
 
 	return (
-		<div>
-			<p></p>
-			<GenerateSubtitlesButton disabled={isFormDisabled} />
+		<div className='generate-subtitles-block'>
+			{mainConfig.region === 'intl' && (
+				<>
+					<p>Если у вас нет субтитров, то вы можете их сгенерировать. Это расходует баланс.</p>
+					<p>Сгенерировать субтитры можно для видео с длительностью не более 1 часа.</p>
+					<GenerateSubtitlesButton disabled={isFormDisabled} />
+				</>
+			)}
+			<p>
+				Бесплатно сгенерировать транскрипцию для видео продолжительностью до 30 минут можно на{' '}
+				<a href='https://turboscribe.ai' className='link'>
+					turboscribe.ai
+				</a>
+				.
+			</p>
 		</div>
 	)
 }
