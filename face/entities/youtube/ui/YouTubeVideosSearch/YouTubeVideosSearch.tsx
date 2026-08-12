40f6feab21bb2a8d880b28471ec6379ed83ebe9a
Andrew Kozinsky <andkozinskiy@yandex.ru>
2026-08-12 15:05:13 +0500

    27

--- face/entities/youtube/ui/YouTubeVideosSearch/YouTubeVideosSearch.tsx
+++ face/entities/youtube/ui/YouTubeVideosSearch/YouTubeVideosSearch.tsx
@@ -1,14 +1,12 @@
 'use client'
 
-import React, { useMemo, useState } from 'react'
-// import { YoutubeApi } from '@/entities/youtube/repository/YoutubeApi'
+import React, { useState } from 'react'
 import YouTubeSearch from '@/entities/youtube/ui/YouTubeSearch/YouTubeSearch'
-// import YouTubeVideosCenterWrapper from '@/entities/youtube/ui/YouTubeVideosCenterWrapper/YouTubeVideosCenterWrapper'
-// import { getConfig } from '@/entities/youtube/ui/YouTubeVideosList/fn/getConfig'
-// import YouTubeVideosList from '@/entities/youtube/ui/YouTubeVideosList/YouTubeVideosList'
+import Button from '@/shared/ui/formRelated/buttons/Button/Button'
+import YouTubeVideosCenterWrapper from '@/widgets/video/YouTubeVideosCenterWrapper/YouTubeVideosCenterWrapper'
+import YouTubeVideosList from '@/widgets/video/YouTubeVideosList/YouTubeVideosList'
 import { useYouTubeVideos } from '_pages/media/youTubeVideos/YouTubeVideosPage/fn/setupYouTubeDeps'
 import './YouTubeVideosSearch.scss'
-import YouTubeVideosList from '@/widgets/video/YouTubeVideosList/YouTubeVideosList'
 
 function YouTubeVideosSearch() {
 	const [query, setQuery] = useState('')
@@ -19,21 +17,6 @@ function YouTubeVideosSearch() {
 		<div className='youtube-videos-search'>
 			<YouTubeSearch query={query} onQueryChange={setQuery} onSearch={search} loading={loading} />
 			<YouTubeVideosList items={videos} loading={loading} error={error} />
-
-			{/*{error && (
-				<YouTubeVideosCenterWrapper>
-					<ErrorMessage text={error} />
-				</YouTubeVideosCenterWrapper>
-			)}
-
-			{loading && (
-				<YouTubeVideosCenterWrapper>
-					<Spinner size='small' />
-				</YouTubeVideosCenterWrapper>
-			)}
-
-			{!loading && videos.length > 0 && <YouTubeVideosList items={getConfig(videos)} />}
-
 			{!loading && hasMore && (
 				<YouTubeVideosCenterWrapper>
 					<Button onClick={loadMore} disabled={loading} loading={loading}>
@@ -41,12 +24,6 @@ function YouTubeVideosSearch() {
 					</Button>
 				</YouTubeVideosCenterWrapper>
 			)}
-
-			{hasSearched && !loading && !error && videos.length === 0 && (
-				<YouTubeVideosCenterWrapper>
-					<ErrorMessage text='Видео не найдены.' />
-				</YouTubeVideosCenterWrapper>
-			)}*/}
 		</div>
 	)
 }
